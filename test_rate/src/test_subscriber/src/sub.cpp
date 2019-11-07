7a4fa8e4ae361f7faf350a295c942398e42a353a
AndrewGavril <gavrilov.andrew1999@yandex.ru>
2019-11-07 15:21:40 +0200

    added parameter

--- test_rate/src/test_subscriber/src/sub.cpp
+++ test_rate/src/test_subscriber/src/sub.cpp
@@ -1,7 +1,7 @@
 #include <chrono>
 #include <fstream>
 #include <string>
-
+#include <vector>
 #include <unistd.h>
 #include <sched.h>
 
@@ -12,33 +12,40 @@
 class Subscriber : public rclcpp::Node
 {
   public:
-    Subscriber(): Node("test_subscriber")
+    Subscriber(char *add_to_cpuset): Node("test_subscriber"),id(getpid())
     {
-      RCLCPP_INFO(this->get_logger(), "I STARTED, %u", getpid());
-      subscription_ = this->create_subscription<std_msgs::msg::String>("test_topic", 1000, std::bind(&Subscriber::callback, this, std::placeholders::_1));
-      id = getpid();
-      std::ofstream f_task("/sys/fs/cgroup/cpuset/sub_cpuset/tasks", std::ios_base::out);
-      if(!f_task.is_open()){
-        RCLCPP_INFO(this->get_logger(), "FILE OPEN ERORR");
+      subscription_ = this->create_subscription<std_msgs::msg::String>("test_topic", 100, std::bind(&Subscriber::callback, this, std::placeholders::_1));
+      if(std::string(add_to_cpuset) == "true"){
+        std::ofstream f_task("/sys/fs/cgroup/cpuset/sub_cpuset/tasks", std::ios_base::out);
+        if(!f_task.is_open()){
+          RCLCPP_INFO(this->get_logger(), "FILE OPEN ERROR");
+        }
+        else{
+          auto s = std::to_string(id);
+          f_task.write(s.c_str(),s.length());
+        }
+        f_task.close();
       }
-      else{
-        auto s = std::to_string(id);
-        f_task.write(s.c_str(),s.length());
-      }
-      f_task.close();
     }
+
+  ~Subscriber(){
+
+    std::ofstream file(std::string(get_name())+".txt");
+    for(unsigned i=0; i<time_list.size();i++)
+      file << time_list[i] << std::endl;
+    file.close();
+  }
 
   private:
-    void callback(const std_msgs::msg::String::SharedPtr msg) const
+    void callback(std_msgs::msg::String::SharedPtr msg)
     {
       auto rec_time = std::chrono::high_resolution_clock::now();
-      auto ns = std::chrono::duration_cast<std::chrono::nanoseconds>(rec_time.time_since_epoch());
-      std::ofstream file("RecTime.txt", std::ios::app);
-      file << ns.count() << std::endl;
-      file.close();
-      RCLCPP_INFO(this->get_logger(), "I heard: '%s'\n Time: %lld\n\n", msg->data.c_str(), ns);
+      auto ns = std::chrono::duration_cast<std::chrono::nanoseconds>(rec_time.time_since_epoch()).count();
+      time_list.push_back(ns);
+      RCLCPP_INFO(this->get_logger(), "I heard: '%s - %lu'", msg->data.c_str(), time_list.size());
     }
 
+    std::vector<unsigned long int> time_list;
     cpu_set_t my_set;
     pid_t id;
     rclcpp::Subscription<std_msgs::msg::String>::SharedPtr subscription_;
@@ -47,7 +54,7 @@ class Subscriber : public rclcpp::Node
 int main(int argc, char * argv[])
 {
   rclcpp::init(argc, argv);
-  rclcpp::spin(std::make_shared<Subscriber>());
+  rclcpp::spin(std::make_shared<Subscriber>(argv[1]));
   rclcpp::shutdown();
   return 0;
 }
