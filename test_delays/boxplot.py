c923c638cbb92c9ccc2e2fbfd8aa7f681388d0c5
marlen99 <marlen.ber@mail.ru>
2019-11-24 20:28:28 +0000

    added plots for delays and message counts in queue

--- test_delays/boxplot.py
+++ test_delays/boxplot.py
@@ -12,10 +12,15 @@ def check_size(i,common):
 	return count_qmsgs
 
 def queue_size(sent, recieved):
-	sent_list = [[sent[0][i], 'pub'] for i in range(0, len(sent[0]))][0:50]
-	recieved_list = [[recieved[0][i], 'sub'] for i in range(0, len(sent[0]))][0:10]
+	sent_list = [[sent[0][i], 'pub'] for i in range(0, len(sent[0]))]
+	recieved_list = [[recieved[0][i], 'sub'] for i in range(0, len(sent[0]))]
 	common = sorted(sent_list + recieved_list, key = lambda el: el[0])
-	list_counts = [check_size(i, common) for i in range(0, len(common))]
+	list_ = [check_size(i, common) for i in range(0, len(common))]
+	list_counts = []
+	for i in range(0, len(list_)):
+		if common[i][1] == 'pub':
+			list_counts.append(list_[i])
+	return list_counts
 
 def main():
 	send_time = []
@@ -39,16 +44,26 @@ def main():
 			print(len(send_time[i]), len(receive_time[i]))
 			print("Number of observations doesn't match")
 			exit()
-	queue_size(send_time,receive_time)
+	list_counts = queue_size(send_time,receive_time)
 	delay_time =[[receive_time[i][j] - send_time[i][j] for j in range(0, len(send_time[i]))] for i in range(0, len(send_time))]
 	delay = []
 	for i in range(0, 10):
 		delay.append(delay_time[0][0:(1000*(i+1))])
 	delay_time = delay
-	means = [sum(delay_time[i])/len(delay_time[i]) for i in range(0, len(send_time))]
-	plt.boxplot(list(map(lambda x: [x[i]/1000000000 for i in range(0, len(x))], delay_time)), positions=[len(d) for d in delay], widths=[500 for i in range(0, 10)])
+	means = [sum(delay_time[i])/(1000000000*len(delay_time[i])) for i in range(0, len(delay_time))]
+	with open('delays.txt', 'w') as f:
+		f.write(' '.join([str(i/1000000000) for i in delay_time[-1]]))
+	plt.boxplot(list(map(lambda x: [x[i]/1000000000 for i in range(0, len(x))], delay_time)), positions=[len(d) for d in delay], widths=[500 for i in range(0, 10)], usermedians=means)
 	plt.ylabel('time, sec')
 	plt.xlabel('message count')
+	plt.show()
+	plt.plot([i for i in range(1, len(delay_time[-1])+1)], list(map(lambda x: x/1000000000, delay_time[-1])))
+	plt.ylabel('time, sec')
+	plt.xlabel('message number')
+	plt.show()
+	plt.plot([i for i in range(1, len(list_counts)+1)], list_counts)
+	plt.ylabel('messages in queue')
+	plt.xlabel('sended message number')
 	plt.show()
 
 if __name__ == '__main__':
