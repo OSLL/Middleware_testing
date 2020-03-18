d817d820454984e1b4f58fbe0fea45ba29db7992
AndrewGavril <gavrilov.andrew1999@yandex.ru>
2020-03-18 14:06:52 +0200

    pub and sub changes for new abstract class

--- OpenSplice/test/src/sub.cpp
+++ OpenSplice/test/src/sub.cpp
@@ -6,73 +6,54 @@
 #include <vector>
 #include <unistd.h>
 #include "gen/TestData_DCPS.hpp"
+#include "test_interface.hpp"
 
 
-class TestSubscriber{
-    dds::domain::DomainParticipant dp;
-    dds::topic::Topic <TestDataType> topic;
-    dds::sub::Subscriber sub;
-    dds::sub::DataReader <TestDataType> dr;
-    dds::core::QosProvider provider;
-    sched_param priority;
-    std::vector<unsigned long int> sub_time;
-
+class TestSubscriber: public TestMiddlewareSub{
 public:
-    TestSubscriber(): dp(org::opensplice::domain::default_id()),
-                        topic(dp, "TestTopic"),
-                        sub(dp),
-                        dr(sub, topic),
-                        provider("file://QoS.xml", "TestProfile"),
-                        sub_time(5000)
-                        {
-                            pid_t id = getpid();
-                            std::ofstream f_task("/sys/fs/cgroup/cpuset/sub_cpuset/tasks", std::ios_base::out);
-                            if(!f_task.is_open()){
-                                std::cout << "Erorr in adding to cpuset"<< std::endl;
-                            }
-                            else{
-                                auto s = std::to_string(id);
-                                f_task.write(s.c_str(),s.length());
-                            }
-                            f_task.close();
-                            priority.sched_priority = sched_get_priority_max(SCHED_FIFO);
-                            int err = sched_setscheduler(id, SCHED_FIFO, &priority);
-                            if(err)
-                                std::cout << "Erorr in setting priority: "<< -err << std::endl;
-                        }
-    int StartTest(int count = 5000){
+    TestSubscriber(std::string topic,  int msgCount=0, int prior=-1, int cpu_index=-1):
+            TestMiddlewareSub(topic, msgCount, prior, cpu_index),
+            _dp(org::opensplice::domain::default_id()),
+            _topic(_dp,topic),
+            _subscriber(_dp),
+            _dr(_subscriber, _topic)
+            {
+                setQoS("file://QoS.xml");
+            }
+
+
+    void setQoS(std::string filename) override {
+        dds::core::QosProvider provider(filename, "TestProfile");
+    }
+
+    int receive() override {
         int i = 0;
-        dds::core::Time time;
-        dds::sub::SampleInfo info;
-        while(i < count){
-            auto samples = dr.read();
-            for(auto j=samples.begin(); j != samples.end(); ++j){
-                sub_time[i] = std::chrono::duration_cast<std::chrono::nanoseconds>(std::chrono::high_resolution_clock::now().time_since_epoch()).count();
-                //info = j->info();
-                //time = info.timestamp();
-                //pub_time[i] = time.nanosec();
-                i++;
+        while(i < _msgCount){
+            auto samples = _dr.read();
+            for(auto j=samples.begin();  j != samples.end(); ++j){
+                if(j->info().state().sample_state() == dds::sub::status::SampleState::not_read()){
+                    msgs[i]=std::string(j->data().data().begin(), j->data().data().end());
+                    i++;
+                }
             }
         }
-        return 0;
+        return i;
     }
 
-    ~TestSubscriber(){
-        std::ofstream file1("subscriber.txt", std::ofstream::app);
-        for(unsigned i=0; i<sub_time.size();i++)
-            file1 << sub_time[i] << ' ';
-        file1 << std::endl;
-        file1.close();
-    }
+private:
+    dds::domain::DomainParticipant _dp;
+    dds::topic::Topic <TestDataType> _topic;
+    dds::sub::Subscriber _subscriber;
+    dds::sub::DataReader <TestDataType> _dr;
 };
 
 int main(int argc, char **argv) {
     try {
-        TestSubscriber subscriber;
-        return subscriber.StartTest();
-
+        TestSubscriber subscriber("/topic", 500);
+        subscriber.StartTest();
     }
     catch (...){
+        std::cout<< "Error!\n";
         return -1;
     }
 }
