3b1cdc76bfa191c4be4b659999ad966e6a490d9a
marlen99 <marlen.ber@mail.ru>
2020-03-18 20:59:36 +0000

    added getting params from json file

--- FastRTPS/test/main.cpp
+++ FastRTPS/test/main.cpp
@@ -2,6 +2,7 @@
 #include "TestSubscriber.h"
 #include "FastRTPSTestPub.h"
 #include "FastRTPSTestSub.h"
+#include "nlohmann/json.hpp"
 
 #include <fastrtps/Domain.h>
 
@@ -12,17 +13,17 @@ using namespace rtps;
 int main(int argc, char** argv)
 {
     int type = 0;
-    char* topic = nullptr;
-    const char* res_filename = "res.json";
+    std::string topic;
+    std::string res_filename = "res.json";
     int m_count = 5000;
     int priority = -1;
     int cpu_index = -1;
     int min_msg_size = 0;
     int max_msg_size = 64000;
     int step = 0;
-    int interval = 0;
     int msgs_before_step = 100;
-
+    int interval = 0;
+    
     if (argc > 1)
     {
         if (strcmp(argv[1], "publisher") == 0)
@@ -34,36 +35,7 @@ int main(int argc, char** argv)
             type = 2;
         }
     }
-    if(argc > 2) {
-	topic = argv[2];
-    }
-    if(argc > 3) {
-        res_filename = argv[3];
-    }
-    if (argc > 4) {
-        m_count = atoi(argv[4]);
-    }
-    if(argc > 5) {
-        min_msg_size = atoi(argv[5]);
-    }
-    if(argc > 6) {
-        max_msg_size = atoi(argv[6]);
-    }
-    if(argc > 7) {
-        step = atoi(argv[7]);
-    }
-    if(argc > 8) {
-        msgs_before_step = atoi(argv[8]);
-    }
-    if(argc > 9) {
-        priority = atoi(argv[9]);
-    }
-    if(argc > 10) {
-        cpu_index = atoi(argv[10]);
-    }
-    if(argc > 11) {
-        interval = atoi(argv[11]);
-    }
+
     if (type==0 || argc < 3)
     {
         std::cout << "Error: Incorrect arguments." << std::endl;
@@ -71,6 +43,47 @@ int main(int argc, char** argv)
         std::cout << argv[0] << " publisher|subscriber topic [res_filename] [m_count] [min_msg_size] [max_msg_size] [step] [msgs_before_step] [priority] [cpu_index] [interval]" << std::endl << std::endl;
         return 0;
     }
+
+    nlohmann::json args;
+    std::ifstream file(argv[2]);
+    if(!file.is_open()) {
+        std::cout << "Cannot open file " << argv[2] << std::endl;
+        return 0;
+    }
+    file >> args;
+	
+    if(args["topic"] != nullptr){
+        topic = args["topic"];
+    }
+    if(args["res_filename"] != nullptr){
+        res_filename = args["res_filename"];
+    }
+    if(args["m_count"] != nullptr){
+        m_count = args["m_count"];
+    }
+    if(args["min_msg_size"] != nullptr){
+        min_msg_size = args["min_msg_size"];
+    }
+    if(args["max_msg_size"] != nullptr){
+        max_msg_size = args["max_msg_size"];
+    }
+    if(args["step"] != nullptr){
+        step = args["step"];
+    }
+    if(args["msgs_before_step"] != nullptr){
+        msgs_before_step = args["msgs_before_step"];
+    }
+    if(args["priority"] != nullptr){
+        priority = args["priority"];
+    }
+    if(args["cpu_index"] != nullptr){
+        cpu_index = args["cpu_index"];
+    }
+    if(args["interval"] != nullptr){
+        interval = args["interval"];
+    }
+
+    file.close();
 
     switch(type)
     {
