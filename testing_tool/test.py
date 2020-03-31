3861accebde0a5fc67400a695bb764d98c61d9ca
marlen99 <marlen.ber@mail.ru>
2020-03-31 17:08:11 +0000

    fixed param names

--- testing_tool/test.py
+++ testing_tool/test.py
@@ -5,8 +5,8 @@ import json
 import time
 
 class MiddlewareTesting(unittest.TestCase):
-    pubs = ["../without_middle_class/build/FastRTPSTest publisher"]
-    subs = ["../without_middle_class/build/FastRTPSTest subscriber"]
+    pubs = ["../FastRTPS/test/build/FastRTPSTest publisher"]
+    subs = ["../FastRTPS/test/build/FastRTPSTest subscriber"]
 
     def test0(self):
         print(">>> running test0")
@@ -15,13 +15,13 @@ class MiddlewareTesting(unittest.TestCase):
         except OSError:
             None
         mlen = [50, 60000]
-        args = {"topic":'test_topic', "res_filename":'json', "m_count":5000, "min_msg_size":100, "max_msg_size":100,  "step":0, "msgs_before_step":5000, "priority":1, "cpu_index":0}
+        args = {"topics":['test_topic'], "res_filenames":['json'], "m_count":5000, "min_msg_size":100, "max_msg_size":100,  "step":0, "msgs_before_step":5000, "priority":1, "cpu_index":0}
         for i in mlen:
             print(" >> message lenght = " + str(i))
             args["min_msg_size"] = i
             args["max_msg_size"] = i
             for j in range(0, len(self.pubs)):
-                args["res_filename"] = 'test0/' + self.subs[j][self.subs[j].rfind('/')+1:self.subs[j].rfind(' ')] + str(i) + '.json'
+                args["res_filenames"] = ['test0/' + self.subs[j][self.subs[j].rfind('/')+1:self.subs[j].rfind(' ')] + str(i) + '.json']
                 with open('args.json', 'w') as f:
                     json.dump(args, f)
                 s = subprocess.Popen(self.subs[j]+' '+'args.json', shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, close_fds=True, cwd=".")
@@ -45,7 +45,7 @@ class MiddlewareTesting(unittest.TestCase):
             os.mkdir("test2")
         except OSError:
             None
-        args = {"topic":'test_topic', "res_filename":'json', "m_count":1000, "min_msg_size":50, "max_msg_size":50,  "step":0, "msgs_before_step":1000, "priority":1, "cpu_index":0}
+        args = {"topics":['test_topic'], "res_filenames":['json'], "m_count":1000, "min_msg_size":65536, "max_msg_size":65536,  "step":0, "msgs_before_step":1000, "priority":1, "cpu_index":0, "interval":50}
         sub_count = range(1, 21)
         for i in sub_count:
             print(" >> number of subscribers - " + str(i))
@@ -57,7 +57,7 @@ class MiddlewareTesting(unittest.TestCase):
                 subs = []
                 for j in range(0, i):
                     time.sleep(0.1)
-                    args["res_filename"] = 'test2/' + str(i) + '/' + self.subs[k][self.subs[k].rfind('/')+1:self.subs[j].rfind(' ')] + str(j) + '.json'
+                    args["res_filenames"] = ['test2/' + str(i) + '/' + self.subs[k][self.subs[k].rfind('/')+1:self.subs[k].rfind(' ')] + str(j) + '.json']
                     with open('args.json', 'w') as f:
                         json.dump(args, f)
                     subs.append(subprocess.Popen(self.subs[k]+' '+'args.json', shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, close_fds=True, cwd="."))
@@ -69,6 +69,7 @@ class MiddlewareTesting(unittest.TestCase):
                 for s in subs:
                     if s.poll() is None:
                         s.wait()
+                    print(s.stdout.readlines())
                     s.stdout.close()
                     s.stdin.close()
                 if p.poll() is None:
