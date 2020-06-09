0caab37aebc2ebf41292c3469ffa309994449bfc
marlen99 <marlen.ber@mail.ru>
2020-06-09 18:01:54 +0300

    changed initial size of msgs

--- testing_tool/test7_config.py
+++ testing_tool/test7_config.py
@@ -6,7 +6,8 @@ def test7_config():
     configs = []
     mkdir_config(7)
     args = {"topic":['test_topic', 'test_topic1'], "res_filenames":['pub', 'sub'], 
-            "m_count":800, "min_msg_size":-896, "max_msg_size":2*1024*1024, 
+            "m_count":800, "min_msg_size":128-256*1024, 
+            "max_msg_size":2*1024*1024, 
             "step":256*1024, "msgs_before_step":100, "priority":[-1, -1],
             "cpu_index":[-1, -1], "interval":50, "topic_priority":100}
     args["res_filenames"][0] = constr_resfilename('', 'p')
