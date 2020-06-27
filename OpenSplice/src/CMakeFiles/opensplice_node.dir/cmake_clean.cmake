b975c058cf1ee55ba9cd700393a32238946144f7
Andrew Gavrilov <gavrilov.andrew1999@yandex.ru>
2020-06-28 01:05:03 +0300

    Include fixes

---
+++ OpenSplice/src/CMakeFiles/opensplice_node.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/opensplice_node.dir/opensplice_node.cpp.o"
+  "opensplice_node.pdb"
+  "opensplice_node"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/opensplice_node.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
