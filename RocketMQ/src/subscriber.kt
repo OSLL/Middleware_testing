8aaaeefcb60b8342dfd9ae6c0124ee02112ed269
Andrew <gavrilov.andrew1999@yandex.ru>
2020-07-21 15:20:54 +0300

    Implemented async pub and sub nodes for rocketmq

--- RocketMQ/src/subscriber.kt
+++ RocketMQ/src/subscriber.kt
@@ -1,42 +1,69 @@
+package rocketmq_test
+
 import com.github.ajalt.clikt.output.TermUi.echo
 import org.apache.rocketmq.client.consumer.DefaultMQPushConsumer
 import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyStatus
 import org.apache.rocketmq.client.consumer.listener.MessageListenerConcurrently
 import org.apache.rocketmq.common.consumer.ConsumeFromWhere
 import java_interface.SubscriberInterface
+import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyContext
 
 import org.apache.rocketmq.common.message.MessageExt
+import java.util.*
 
-class Subscriber(val topic: String, val msgCount: Int, val prior: Int, val cpu_index: Int,
-                 val filename: String, val topic_prior: Int):
+class SetFlagTimer(private val listener: Listener): TimerTask(){
+    override fun run() {
+        echo("timer")
+        listener.isReceived = false
+    }
+}
+
+class Listener: MessageListenerConcurrently{
+    var isReceived = false
+    private val timerTask = SetFlagTimer(this)
+    private val timer = Timer(false)
+    override fun consumeMessage(msgs: MutableList<MessageExt>?, context: ConsumeConcurrentlyContext): ConsumeConcurrentlyStatus {
+        return if (msgs == null || msgs.isEmpty()) {
+            timer.schedule(timerTask, 1)
+            echo("Not rec")
+            ConsumeConcurrentlyStatus.RECONSUME_LATER
+        }
+        else {
+            echo("Received New Messages: $msgs")
+            ConsumeConcurrentlyStatus.CONSUME_SUCCESS
+        }
+    }
+}
+
+class Subscriber(topic: String, msgCount: Int, prior: Int, cpu_index: Int,
+                 filename: String, topic_prior: Int):
         SubscriberInterface<MessageExt>(topic, msgCount, prior, cpu_index, filename, topic_prior){
-    val consumer = DefaultMQPushConsumer("subscribers")
+    private val consumer = DefaultMQPushConsumer("subscribers")
+    private val listener = Listener()
     init {
-
-        consumer.setNamesrvAddr("localhost:9876")
+        consumer.namesrvAddr = "localhost:9876"
         consumer.consumeFromWhere = ConsumeFromWhere.CONSUME_FROM_FIRST_OFFSET
         consumer.subscribe("TopicTest", "*")
-        consumer.registerMessageListener(MessageListenerConcurrently { msgs, context ->
-            echo ("Received New Messages: $msgs")
-            ConsumeConcurrentlyStatus.CONSUME_SUCCESS
-        })
+        consumer.registerMessageListener(listener)
         consumer.start()
         echo("Consumer Started.")
     }
 
-    override fun get_id(msg: MessageExt?): Int {
-        val msg = msg!!.body.toString()
-        val id = msg.subSequence(0, msg.indexOf("ts:")).toString().toInt()
-        return id
+    override fun get_id(message: MessageExt?): Int {
+        val msg = message!!.body.toString()
+        return msg.subSequence(0, msg.indexOf("ts:")).toString().toInt()
     }
 
-    override fun get_timestamp(msg: MessageExt?): Long {
-        val msg = msg!!.body.toString()
-        val timestamp = msg.subSequence(msg.indexOf("ts:") + 3, msg.indexOf("data:")).toString().toLong()
-        return timestamp
+    override fun get_timestamp(message: MessageExt?): Long {
+        val msg = message!!.body.toString()
+        return msg.subSequence(msg.indexOf("ts:") + 3, msg.indexOf("data:")).toString().toLong()
     }
 
     override fun receive(): Boolean {
-        TODO("Not yet implemented")
+        return listener.isReceived
+    }
+
+    fun stopNode(){
+        consumer.shutdown()
     }
 }
