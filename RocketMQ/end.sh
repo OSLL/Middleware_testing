5b0b81486458f9cf12366cefd4cdf5ebd38d4137
Andrew <gavrilov.andrew1999@yandex.ru>
2020-08-07 16:24:38 +0300

    fixed start/end scripts

--- RocketMQ/end.sh
+++ RocketMQ/end.sh
@@ -3,8 +3,10 @@ cd $1
 export NAMESRV_ADDR=localhost:9876
 clusters=$(./bin/mqadmin clusterList)
 broker_addr=(${clusters[@]})
+cluster_name=${broker_addr[12]}
 broker_addr=${broker_addr[15]}
-./bin/mqadmin deleteTopic -b $broker_addr -t test_topic
-./bin/mqadmin deleteTopic -b $broker_addr -t test_topic1
+./bin/mqadmin deleteTopic -c $cluster_name -t test_topic
+./bin/mqadmin deleteTopic -c $cluster_name -t test_topic1
+./bin/mqadmin deleteSubGroup -b $broker_addr -g subscribers
 sh bin/mqshutdown broker
 sh bin/mqshutdown namesrv
--- RocketMQ/start.sh
+++ RocketMQ/start.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 cd $1
 nohup sh bin/mqnamesrv &
-sleep 1
-nohup sh bin/mqbroker -n localhost:9876 &
-sleep 3
+sleep 5
 export NAMESRV_ADDR=localhost:9876
+nohup sh bin/mqbroker -n localhost:9876 &
+sleep 10
 clusters=$(./bin/mqadmin clusterList)
 broker_addr=(${clusters[@]})
 broker_addr=${broker_addr[15]}
-./bin/mqadmin updateTopic -b $broker_addr -t test_topic
-./bin/mqadmin updateTopic -b $broker_addr -t test_topic1
+./bin/mqadmin updateTopic -b $broker_addr -o true -t test_topic
+./bin/mqadmin updateTopic -b $broker_addr -o true -t test_topic1
