1a57d2dc1b28795965364ca58ee734d71a8f5318
AndrewGavril <gavrilov.andrew1999@yandex.ru>
2020-12-13 16:40:36 +0300

    Cmake fix

--- ZCM/include/subscriber.h
+++ ZCM/include/subscriber.h
@@ -1,8 +1,30 @@
-//
-// Created by andrew on 12.12.2020.
-//
+#pragma once
+#include <atomic>
+#include <zcm/zcm-cpp.hpp>
+#include "../../interface/sub_interface.hpp"
+#include "../include/msg_t.hpp"
 
-#ifndef ZCM_NODE_SUBSCRIBER_H
-#define ZCM_NODE_SUBSCRIBER_H
+class TestSubscriber : public TestMiddlewareSub<msg_t>
+{
+public:
 
-#endif //ZCM_NODE_SUBSCRIBER_H
+    TestSubscriber(std::string &topic, int msgCount, int prior, int cpu_index, std::string &filename, int topic_prior, int max_msg_size);
+
+    virtual ~TestSubscriber();
+
+    bool receive();
+
+    short get_id(msg_t &msg);
+
+    unsigned long get_timestamp(msg_t &msg);
+
+private:
+    void handleMessage(const zcm::ReceiveBuffer *rbuf,
+                       const std::string &chan,
+                       const msg_t *msg);
+
+    std::atomic_bool isReceived = false;
+
+    zcm::ZCM zcm;
+};
+
